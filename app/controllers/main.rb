f3470bf74d34e8d77ad1dc12f1f5b91304d8799a
Theo Vora <theevo@gmail.com>
2013-10-31 14:44:02 -0400

    work with env variables

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -1,7 +1,12 @@
 class MainApplication < Sinatra::Base
+  use Rack::Auth::Basic, "Restricted Area" do |username, password|
+    username == WEB_USER and password == WEB_PASSWORD
+  end
+
   set :views, File.expand_path('../../views', __FILE__)
 
   get '/' do
+    @time = Time.now
     @client = Octokit::Client.new \
       :login => MY_LOGIN,
       :password => MY_PASSWORD
--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,12 @@ require 'bundler'
 
 Bundler.require
 
-require_relative './login.rb'
 require_relative '../app/controllers/main.rb'
+
+MY_LOGIN = ENV['CAMELBACK_MY_LOGIN']
+MY_PASSWORD = ENV['CAMELBACK_MY_PASSWORD']
+MY_ORGANIZATION = ENV['CAMELBACK_MY_ORGANIZATION']
+WEB_USER = ENV['CAMELBACK_WEB_USER']
+WEB_PASSWORD = ENV['CAMELBACK_WEB_PASSWORD']
+
+MY_ORG_CAPITALIZE = MY_ORGANIZATION.split('-').map { |s| s.capitalize }.join(' ')
